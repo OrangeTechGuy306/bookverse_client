165a0fb34663d5cb345ac2051a3cba7f26331570
orange <orangetech@gmail.com>
2025-04-29 08:10:28 +0100

    changing to production api

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,5 +1,5 @@
 
-// export const API_URL = 'https://www.ebook.pinetverify.com/api/v1'
-export const API_URL = 'http://localhost:5000/api/v1'
-// export const IMAGE_URL = 'https://www.ebook.pinetverify.com/uploads'
-export const IMAGE_URL = 'http://localhost:5000/uploads'
+export const API_URL = 'https://www.ebook.pinetverify.com/api/v1'
+export const IMAGE_URL = 'https://www.ebook.pinetverify.com/uploads'
+// export const API_URL = 'http://localhost:5000/api/v1'
+// export const IMAGE_URL = 'http://localhost:5000/uploads'
